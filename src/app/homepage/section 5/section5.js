ff952f8eb2c2bdcfa85c7829f8d5fea8784e0a75
JadaRose237 <rosejada420@gmail.com>
2024-11-20 10:32:26 +0000

    starting section 5

---
+++ src/app/homepage/section 5/section5.js
@@ -0,0 +1,10 @@
+"use client";
+
+import React, { useState } from "react";
+import styles from "./section5.module.css";
+import Link from "next/link"; 
+
+export default function Section5() {
+
+    
+}
